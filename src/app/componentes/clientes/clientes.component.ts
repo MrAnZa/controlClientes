11903af2cde4e941883815f1b68f53d78a20961d
Andrés Zamora <andreszamora1998@gmail.com>
2019-04-30 22:59:59 -0500

    Agregar Cliente Parte 2

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ClienteServicio} from 'src/app/servicios/clientes.service';
 import {Cliente} from 'src/app/modelo/cliente.model';
+import {FlashMessagesService} from 'angular2-flash-messages';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -8,7 +9,16 @@ import {Cliente} from 'src/app/modelo/cliente.model';
 })
 export class ClientesComponent implements OnInit {
 	clientes: Cliente[];
-  constructor(private clientesServicio: ClienteServicio) { }
+
+  cliente: Cliente={
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+    }
+
+  constructor(private clientesServicio: ClienteServicio,
+              private flashMessages: FlashMessagesService) { }
 
   ngOnInit() {
   	this.clientesServicio.getclientes().subscribe(
@@ -26,5 +36,14 @@ export class ClientesComponent implements OnInit {
       })
     }
   return saldoTotal;
+  }
+  agregar({value,valid}: {value: Cliente, valid: boolean}){
+    if(!valid){
+      this.flashMessages.show('Por Favor LLena el Formulario Correctamente',{
+        cssClass: 'alert-danger',timeout:4000
+      });
+    }else{
+      
+    }
   }
 }
