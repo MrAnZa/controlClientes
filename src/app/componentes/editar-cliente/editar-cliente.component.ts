437567a66001047302378faff1f6dab81965636f
Andrés Zamora <andreszamora1998@gmail.com>
2019-05-02 19:18:46 -0500

    Modificar y Eliminar Cliente Parte 1

--- src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ClienteServicio} from 'src/app/servicios/clientes.service';
+import {Cliente} from 'src/app/modelo/cliente.model';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router,ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-editar-cliente',
   templateUrl: './editar-cliente.component.html',
   styleUrls: ['./editar-cliente.component.css']
 })
 export class EditarClienteComponent implements OnInit {
+  cliente: Cliente={
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+    }
 
-  constructor() { }
+    id:string;
+ constructor(private clientesServicio: ClienteServicio,
+              private flashMessages: FlashMessagesService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.id=this.route.snapshot.params['id'];
+  	this.clientesServicio.getCliente(this.id).subscribe(cliente=>{
+  		this.cliente=cliente;
+  	})
   }
 
 }
